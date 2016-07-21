aec26d7c46f141050de007a10ae6b284328460f8
QuoniamYIF <quoniamy@gmail.com>
2016-07-21 13:59:57 +0800

    at chaos

--- app.js
+++ app.js
@@ -1,14 +1,24 @@
 var Koa = require('koa');
+var path = require('path');
+var util = require('./libs/util');
 var wechat = require('./access');
+var wechat_file = path.join(__dirname, './config/wechat.txt');
 var config = {
     wechat: {
         appID:'wx4eb23e853c517871',
         appSecret: '9cb37d4aed3618e55a9e700bf94485b5',
-        token: 'quoniammm'
+        token: 'quoniammm',
+        getAccessToken: function() {
+            return util.readFileAsync(wechat_file);
+        },
+        saveAcessToken: function(data) {
+            data = JSON.parse(data);
+            return util.writeFileAsync(wechat_file, data);
+        }
     }
 }
 var app = new Koa();
 
-app.use(wechat(config));
+app.use(wechat(config.wechat));
 
 app.listen(80);
